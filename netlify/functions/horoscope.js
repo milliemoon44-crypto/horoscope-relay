36cbfadf3dcbdd05d09466540a0e916a4750aa3c
milliemoon44-crypto <milliemoon44@gmail.com>
2025-11-15 18:40:18 -0600

    Fix parameter name from sign to zodiac

--- netlify/functions/horoscope.js
+++ netlify/functions/horoscope.js
@@ -1,7 +1,7 @@
 export async function handler(event) {
-  const sign = event.queryStringParameters.sign || "aries";
-  const apiKey = "nklqSaiR/RBkp3IP/7MGiQ==fYyUf2ZPFARjzogQ"; // your API key
-  const url = `https://api.api-ninjas.com/v1/horoscope?sign=${sign}`;
+  const zodiac = event.queryStringParameters.zodiac || "aries";
+  const apiKey = "nklqSaiR/RBkp3IP/7MGiQ==fYyUf2ZPFARjzogQ";
+  const url = `https://api.api-ninjas.com/v1/horoscope?sign=${zodiac}`;
 
   try {
     const res = await fetch(url, { headers: { 'X-Api-Key': apiKey }});
